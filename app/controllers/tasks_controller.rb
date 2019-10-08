992d99560bd86ef4c356af144e0167708491fac9
ashidasatoshi <dasatoshi2017@yahoo.co.jp>
2019-10-08 13:31:40 +0900

    コントローラー、ビュー実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,53 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    #Task.create(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスク作成しました"
+    else
+      render :new
+    end
+    #redirect_to new_task_path
   end
 
   def show
+    #@task = Task.find(params[:id])
   end
 
   def edit
+    #@task = Task.find(params[:id])
   end
 
-  de
+  def update
+    #@task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"削除しました！"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
