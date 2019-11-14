dd1dc17e3646ce6b3f92ef6b8bcea7e27a8b7dcf
ashidasatoshi <dasatoshi2017@yahoo.co.jp>
2019-11-14 11:49:17 +0900

    NotNull制約、バリデーションに対してmodelテスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_092756) do
+ActiveRecord::Schema.define(version: 2019_11_14_024034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
+    t.string "name", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
